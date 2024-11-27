8b6c04814e48d5eced1c839be9bac029fae7a3bb
Matt Vaughn <matt.vaughn@buildmotion.com>
2024-11-27 11:40:51 -0700

    add context to state machine; default value

--- workspace/libs/toy-robot-service/src/lib/machine.spec.ts
+++ workspace/libs/toy-robot-service/src/lib/machine.spec.ts
@@ -15,75 +15,90 @@ export const STATES = {
   CHASING: 'chasing' as const,
 };
 
+export const EVENTS = {
+  PLAY: 'PLAY' as const,
+  STOP: 'STOP' as const,
+  EAT: 'EAT' as const,
+  FETCH: 'FETCH' as const,
+  CHASE: 'CHASE' as const,
+  MUD: 'MUD' as const,
+};
+
 type StateType = (typeof STATES)[keyof typeof STATES];
+type EventType = (typeof EVENTS)[keyof typeof EVENTS];
 
 const workflow = setup({
   types: {
-    context: {} as {},
-    events: {} as { type: 'PLAY' | 'STOP' | 'EAT' | 'FETCH' | 'CHASE' | 'MUD' },
+    context: {} as LukkaContext,
+    events: {} as { type: EventType },
   },
   actions: {},
 }).createMachine({
   id: 'Lukka',
-  initial: STATES.IDLE,
+  initial: STATES.IDLE as StateType,
+  context: {
+    name: 'Lukka',
+    age: 9,
+    status: 'happy',
+  },
   states: {
-    [STATES.IDLE]: {
+    [STATES.IDLE as StateType]: {
       on: {
-        PLAY: {
-          target: STATES.PLAYING,
+        [EVENTS.PLAY as EventType]: {
+          target: STATES.PLAYING as StateType,
         },
-        EAT: {
-          target: STATES.EATING,
+        [EVENTS.EAT as EventType]: {
+          target: STATES.EATING as StateType,
         },
       },
     },
-    [STATES.EATING]: {},
-    [STATES.PLAYING]: {
-      initial: STATES.MUDDING,
+    [STATES.EATING as StateType]: {},
+    [STATES.PLAYING as StateType]: {
+      initial: STATES.MUDDING as StateType,
       states: {
-        [STATES.MUDDING]: {
+        [STATES.MUDDING as StateType]: {
           on: {
-            FETCH: {
-              target: STATES.FETCHING,
+            [EVENTS.FETCH as EventType]: {
+              target: STATES.FETCHING as StateType,
             },
-            CHASE: {
-              target: STATES.CHASING,
+            [EVENTS.CHASE as EventType]: {
+              target: STATES.CHASING as StateType,
             },
-            STOP: {
-              target: `#Lukka.${STATES.IDLE}`,
+            [EVENTS.STOP as EventType]: {
+              target: `#Lukka.${STATES.IDLE}` as StateType,
             },
           },
         },
-        [STATES.FETCHING]: {
+        [STATES.FETCHING as StateType]: {
           on: {
-            MUD: {
-              target: STATES.MUDDING,
+            [EVENTS.MUD as EventType]: {
+              target: STATES.MUDDING as StateType,
             },
-            CHASE: {
-              target: STATES.CHASING,
+            [EVENTS.CHASE as EventType]: {
+              target: STATES.CHASING as StateType,
             },
-            STOP: {
-              target: `#Lukka.${STATES.IDLE}`,
+            [EVENTS.STOP as EventType]: {
+              target: `#Lukka.${STATES.IDLE}` as StateType,
             },
           },
         },
-        [STATES.CHASING]: {
+        [STATES.CHASING as StateType]: {
           on: {
-            MUD: {
-              target: STATES.MUDDING,
+            [EVENTS.MUD as EventType]: {
+              target: STATES.MUDDING as StateType,
             },
-            FETCH: {
-              target: STATES.FETCHING,
+            [EVENTS.FETCH as EventType]: {
+              target: STATES.FETCHING as StateType,
             },
-            STOP: {
-              target: `#Lukka.${STATES.IDLE}`,
+            [EVENTS.STOP as EventType]: {
+              target: `#Lukka.${STATES.IDLE}` as StateType,
             },
           },
         },
       },
       on: {
-        STOP: {
-          target: STATES.IDLE,
+        [EVENTS.STOP as EventType]: {
+          target: STATES.IDLE as StateType,
         },
       },
     },
@@ -104,84 +119,93 @@ describe('Lukka Machine', () => {
 
   describe('Initial State', () => {
     it('should start in the [idle] state when workflow starts/initializes', () => {
-      expect(actor.getSnapshot().value).toEqual('idle');
-      expect(actor.getSnapshot().value).toEqual(STATES.IDLE);
+      expect(actor.getSnapshot().value).toEqual(STATES.IDLE as StateType);
+      expect(actor.getSnapshot().context).toEqual({
+        name: 'Lukka',
+        age: 9,
+        status: 'happy',
+      });
+    });
+
+    it('should initialize workflow with default [context]', () => {
+      expect(actor.getSnapshot().context).toEqual({
+        name: 'Lukka',
+        age: 9,
+        status: 'happy',
+      });
     });
 
     it('should transition to [playing] state when PLAY event is triggered', () => {
-      actor.send({ type: 'PLAY' });
-      expect(actor.getSnapshot().value).toEqual({ playing: 'mudding' });
-      expect(actor.getSnapshot().value).toEqual({ [STATES.PLAYING]: STATES.MUDDING });
+      actor.send({ type: EVENTS.PLAY as EventType });
+      expect(actor.getSnapshot().value).toEqual({ [STATES.PLAYING as StateType]: STATES.MUDDING as StateType });
     });
 
     it('should transition to [eating] state when EAT event is triggered', () => {
-      actor.send({ type: 'EAT' });
-      expect(actor.getSnapshot().value).toEqual('eating');
-      expect(actor.getSnapshot().value).toEqual(STATES.EATING);
+      actor.send({ type: EVENTS.EAT as EventType });
+      expect(actor.getSnapshot().value).toEqual(STATES.EATING as StateType);
     });
 
     it('should transition to [idle] state when STOP event is triggered', () => {
-      actor.send({ type: 'PLAY' });
-      actor.send({ type: 'STOP' });
-      expect(actor.getSnapshot().value).toEqual('idle');
-      expect(actor.getSnapshot().value).toEqual(STATES.IDLE);
+      actor.send({ type: EVENTS.PLAY as EventType });
+      actor.send({ type: EVENTS.STOP as EventType });
+      expect(actor.getSnapshot().value).toEqual(STATES.IDLE as StateType);
     });
   });
 
   describe('Playing State Transitions', () => {
     beforeEach(() => {
-      actor.send({ type: 'PLAY' });
+      actor.send({ type: EVENTS.PLAY as EventType });
     });
 
     it('should start in [mudding] sub-state initially', () => {
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.MUDDING,
+        [STATES.PLAYING as StateType]: STATES.MUDDING as StateType,
       });
     });
 
     it('should transition to [fetching] from [mudding] when FETCH event is triggered', () => {
-      actor.send({ type: 'FETCH' });
+      actor.send({ type: EVENTS.FETCH as EventType });
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.FETCHING,
+        [STATES.PLAYING as StateType]: STATES.FETCHING as StateType,
       });
     });
 
     it('should transition to [chasing] from [mudding] when CHASE event is triggered', () => {
-      actor.send({ type: 'CHASE' });
+      actor.send({ type: EVENTS.CHASE as EventType });
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.CHASING,
+        [STATES.PLAYING as StateType]: STATES.CHASING as StateType,
       });
     });
 
     it('should transition to [mudding] from [fetching] when MUD event is triggered', () => {
-      actor.send({ type: 'FETCH' });
-      actor.send({ type: 'MUD' });
+      actor.send({ type: EVENTS.FETCH as EventType });
+      actor.send({ type: EVENTS.MUD as EventType });
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.MUDDING,
+        [STATES.PLAYING as StateType]: STATES.MUDDING as StateType,
       });
     });
 
     it('should transition to [chasing] from [fetching] when CHASE event is triggered', () => {
-      actor.send({ type: 'FETCH' });
-      actor.send({ type: 'CHASE' });
+      actor.send({ type: EVENTS.FETCH as EventType });
+      actor.send({ type: EVENTS.CHASE as EventType });
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.CHASING,
+        [STATES.PLAYING as StateType]: STATES.CHASING as StateType,
       });
     });
 
     it('should transition to [mudding] from [chasing] when MUD event is triggered', () => {
-      actor.send({ type: 'CHASE' });
-      actor.send({ type: 'MUD' });
+      actor.send({ type: EVENTS.CHASE as EventType });
+      actor.send({ type: EVENTS.MUD as EventType });
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.MUDDING,
+        [STATES.PLAYING as StateType]: STATES.MUDDING as StateType,
       });
     });
 
     it('should transition to [fetching] from [chasing] when FETCH event is triggered', () => {
-      actor.send({ type: 'CHASE' });
-      actor.send({ type: 'FETCH' });
+      actor.send({ type: EVENTS.CHASE as EventType });
+      actor.send({ type: EVENTS.FETCH as EventType });
       expect(actor.getSnapshot().value).toEqual({
-        [STATES.PLAYING]: STATES.FETCHING,
+        [STATES.PLAYING as StateType]: STATES.FETCHING as StateType,
       });
     });
   });
